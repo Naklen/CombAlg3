c9c5bf8747f5e33d7b8fbfb805e0353066555510
Naklen <dryagin.ser@yandex.ru>
2020-05-19 01:14:17 +0500

    Implemented algorythm with console output

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net;
+using System.Runtime.InteropServices;
 
 namespace CombAlg3
 {
@@ -14,12 +15,54 @@ namespace CombAlg3
         {
             var data = GetStartData();
             Console.WriteLine("Vertex count: " + data.Item2);
-            Console.WriteLine($"Start: {data.Item3} Target: {data.Item4}");
+            Console.WriteLine($"Start: {data.Item3 + 1} Target: {data.Item4 + 1}");
             foreach (var edge in data.Item1)
                 Console.WriteLine(edge.ToString());
+            Console.WriteLine($"Path from {data.Item3 + 1} to {data.Item4 + 1}:");
+            Console.WriteLine(GetMinPath(data));
             Console.ReadKey();
 
         }
+
+        static string GetMinPath(Tuple<List<Edge>, int, int, int> data)
+        {
+            var edges = data.Item1;
+            var vCount = data.Item2;
+            var start = data.Item3;
+            var target = data.Item4;
+            var minCosts = Enumerable.Repeat(int.MaxValue, vCount).ToArray();
+            var previous = Enumerable.Repeat(-1, vCount).ToArray();
+            minCosts[start] = 0;
+            while (true)
+            {
+                var cont = false;
+                for (int i = 0; i < edges.Count; i++)
+                    if (minCosts[edges[i].From] < int.MaxValue)
+                        if (minCosts[edges[i].To] > minCosts[edges[i].From] + edges[i].Cost)
+                        { 
+                            minCosts[edges[i].To] = minCosts[edges[i].From] + edges[i].Cost;
+                            previous[edges[i].To] = edges[i].From;
+                            cont = true;
+                        }
+                if (!cont) break;
+            }
+            if (minCosts[target] == int.MaxValue)
+                return null;
+            var reversedPath = new Queue<int>();
+            var cur = target;
+            while (cur != -1)
+            {
+                reversedPath.Enqueue(cur);
+                cur = previous[cur];
+            }
+            var result = "";
+            var path = reversedPath.Reverse();
+            foreach (var v in path)
+                result += (v + 1) + " ";
+            result = result.Substring(0, result.Length - 1);
+            return result;
+        }
+
         static Tuple<List<Edge>, int, int, int> GetStartData()
         {
             var edges = new List<Edge>();
@@ -31,17 +74,17 @@ namespace CombAlg3
                 using (var sr = new StreamReader("./in.txt"))
                 {
                     vertexCount = int.Parse(sr.ReadLine());
-                    for (int i = 1; i <= vertexCount; i++)
+                    for (int i = 0; i < vertexCount; i++)
                     {
                         var incidentVertex = sr.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
-                        for (int j = 1; j <= vertexCount; j++)
+                        for (int j = 0; j < vertexCount; j++)
                         {
-                            if (incidentVertex[j - 1] > -32768)
-                                edges.Add(new Edge(i, j, incidentVertex[j - 1]));
+                            if (incidentVertex[j] > -32768)
+                                edges.Add(new Edge(i, j, incidentVertex[j]));
                         }
                     }
-                    start = int.Parse(sr.ReadLine());
-                    target = int.Parse(sr.ReadLine());
+                    start = int.Parse(sr.ReadLine()) - 1;
+                    target = int.Parse(sr.ReadLine()) - 1;
                 }
             }
             catch (Exception e)
